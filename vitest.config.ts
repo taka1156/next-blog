4c481ca1b3fa7d787af722dbb2b2bbfea563e096
taka <tomo05061@gmail.com>
2024-09-15 11:33:41 +0900

    add: テスト環境作成

---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+/// <reference types="vitest" >
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    reporters: ['default', 'html'],
+    coverage: {
+      thresholds: {
+        statements: 80,
+        functions: 80,
+        branches: 80,
+        lines: 80
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@/': __dirname + '/src/'
+    }
+  }
+});
