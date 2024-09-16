d0396dbdefaf1dbe7ca8559ba127eaf178fa72a5
taka <tomo05061@gmail.com>
2024-09-16 16:07:49 +0900

    add: 型の読み込み設定追加、ダミー定義追加

--- src/dummy/index.ts
+++ src/dummy/index.ts
@@ -1,5 +1,223 @@
 const dummyImgUrl = 'https://placehold.jp/150x150.png';
 const dummyUrl = 'https://placehold.jp';
 const dummyCopyrightUrl = 'http://placehold.jp';
+const dummyDay = '2023-12-31';
+const MAX_NAV_DATA = 4;
+const MAX_ICONS_DATA = 4;
+const MAX_ARTICLE_DATA = 10;
+const MAX_BADGE_DATA = 5;
+const MAX_TOC_DATA = 5;
 
-export { dummyUrl, dummyImgUrl, dummyCopyrightUrl };
+const dummyOutsideLink = { routeTo: dummyUrl };
+const dummyRouteStr = { routeTo: '/' };
+const dummyRouteObj = {
+  routeTo: {
+    name: 'sample-id',
+    params: { id: 1 }
+  }
+};
+
+// factory
+const dummyFactory = <T>(num: number, fn: (id: number) => T) => {
+  return [...new Array(num)].map((_, i) => fn(i + 1));
+};
+
+// template
+const dummyNavTemplate = (i: number): RouteItem => ({
+  name: `ダミー${i}`,
+  to: '/test1',
+  img: dummyImgUrl
+});
+
+const dummyBadgeTemplate = (i: number, name: string): CommonBadge => ({
+  id: i.toString(),
+  name: name,
+  img: { url: dummyImgUrl }
+});
+
+const dummyClassificationTemplate = (
+  id: number,
+  name: string
+): CommonClassificationItem => ({
+  id: id.toString(),
+  name: name,
+  img: {
+    url: dummyImgUrl
+  }
+});
+
+const dummyTocTemplate = (i: number) => ({
+  index: i,
+  escapedText: 'この文章はダミーです。',
+  anchor: `anchor_${i}`
+});
+
+const dummyMarkdown = `
+## この文章はダミーです。
+  **文字の大きさ、量、字間、行間等を確認するために入れています。** 
+## この文章はダミーです。 
+  **文字の大きさ、量、字間、行間等を確認するために入れています。** 
+## この文章はダミーです。 
+  **文字の大きさ、量、字間、行間等を確認するために入れています。**
+## この文章はダミーです。
+  **文字の大きさ、量、字間、行間等を確認するために入れています。**
+## この文章はダミーです。
+  **文字の大きさ、量、字間、行間等を確認するために入れています。**
+
+**aaa**
+
+**bbb**
+
+:: どんどん共有しましょう
+*プログラミングに関することをどんどん投稿して、*
+**知識を記録、共有しましょう。**
+_プログラミングに関することをどんどん投稿して、_
+~~知識を記録、共有しましょう。~~
+::
+
+
+## marked.js拡張構文
+
+:: spoiler word
+  **aaa**
+::
+
+:: point ポイント
+  ポイント
+::
+
+:: info 情報
+  インフォメーション
+::
+
+:: warn 注意
+  〇〇に注意
+::
+
+:: alert 警告
+  〇〇に警告
+::
+
+`;
+
+const dummyArticleTemplate = (i: number) => ({
+  id: i,
+  ...dummyDate,
+  title:
+    'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています',
+  summary:
+    'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量',
+  body: dummyMarkdown,
+  tags: dummyTagBadges.slice(0, i),
+  category: dummyBadgeTemplate(1, 'ダミーカテゴリー')
+});
+
+const dummySnsIconTemplate = (i: number) => ({
+  name: `ダミー${i}`,
+  img: dummyImgUrl,
+  link: dummyUrl
+});
+
+// dummyDate
+const dummyImg = {
+  imgUrl: dummyImgUrl,
+  imgAlt: 'ダミーの画像'
+};
+
+const dummyDate = {
+  createdAt: '2020-07-04T10:53:40.252Z',
+  updatedAt: '2020-07-08T15:15:07.668Z'
+};
+
+const dummyRoutes = dummyFactory(MAX_NAV_DATA, (i: number) => dummyNavTemplate(i));
+
+const dummyTagBadges = dummyFactory(MAX_BADGE_DATA, (i: number) =>
+  dummyBadgeTemplate(i, 'ダミータグ')
+);
+
+const dummyClassificationFactory = (
+  items: CommonClassificationItems,
+  routePath: string
+) => {
+  const tmp = items;
+  return {
+    items: tmp,
+    routePath: routePath
+  };
+};
+
+const dummyClassificationCategory = dummyClassificationFactory(
+  dummyFactory(MAX_BADGE_DATA, (id: number) =>
+    dummyClassificationTemplate(id, 'ダミーカテゴリー')
+  ),
+  'category'
+);
+
+const dummyClassificationTag = dummyClassificationFactory(
+  dummyFactory(MAX_BADGE_DATA, (id: number) =>
+    dummyClassificationTemplate(id, 'ダミータグ')
+  ),
+  'tag'
+);
+
+const dummyArticles = dummyFactory(MAX_ARTICLE_DATA, (id: number) =>
+  dummyArticleTemplate(id)
+);
+
+const dummyFactoryBadge = (id: number, name: string, routePath: string) => {
+  const tmp = dummyBadgeTemplate(id, name);
+  return {
+    routePath: routePath,
+    badge: tmp
+  };
+};
+
+const dummyCategoryBadge = dummyFactoryBadge(1, 'ダミーカテゴリー', 'category-id');
+
+const dummyTagBadge = dummyFactoryBadge(1, 'ダミータグ', 'tag-id');
+
+const dummyPagination = {
+  currentPage: 1,
+  maxPage: Math.ceil(dummyArticles.length / 5),
+  routePath: 'page-pageid',
+  prevIndex: 2,
+  nextIndex: 2
+};
+
+const dummyTocs = dummyFactory(MAX_TOC_DATA, (i: number) => dummyTocTemplate(i));
+
+const dummyLogo = 'ダミーロゴ';
+
+const dummySnsIcons = dummyFactory(MAX_ICONS_DATA, (i: number) =>
+  dummySnsIconTemplate(i)
+);
+
+const dummyProfile = {
+  img: dummyImgUrl,
+  introduce:
+    'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。'
+};
+
+export {
+  dummyUrl,
+  dummyImgUrl,
+  dummyOutsideLink,
+  dummyRouteStr,
+  dummyRouteObj,
+  dummyImg,
+  dummyCategoryBadge,
+  dummyTagBadge,
+  dummyTagBadges,
+  dummyDate,
+  dummyArticles,
+  dummyPagination,
+  dummyClassificationCategory,
+  dummyClassificationTag,
+  dummyLogo,
+  dummyTocs,
+  dummyRoutes,
+  dummyCopyrightUrl,
+  dummyDay,
+  dummySnsIcons,
+  dummyProfile
+};
