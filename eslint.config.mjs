7093a59a636f6e238f5355cffc811bcddbecef19
taka <tomo05061@gmail.com>
2026-06-16 04:58:52 +0900

    chore: 設定ファイル見直し

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,46 +1,45 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import _import from "eslint-plugin-import";
-import unusedImports from "eslint-plugin-unused-imports";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import _import from 'eslint-plugin-import';
+import unusedImports from 'eslint-plugin-unused-imports';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
 });
 
-export default defineConfig(
-    [globalIgnores(["**/next.config.js", "**/*.test.js", "**/*.stories.js"]), {
-        extends: fixupConfigRules(compat.extends(
-            "next/core-web-vitals",
-            "plugin:@typescript-eslint/recommended",
-            "plugin:import/recommended",
-            "prettier",
-            "plugin:storybook/recommended",
-        )),
-
-        plugins: {
-            "@typescript-eslint": fixupPluginRules(typescriptEslint),
-            import: fixupPluginRules(_import),
-            "unused-imports": unusedImports,
-        },
-
-        languageOptions: {
-            parser: tsParser,
-        },
-
-        rules: {
-            "@next/next/no-img-element": "off",
-            "unused-imports/no-unused-imports": "error",
-        },
-    }],
-);
+export default defineConfig([
+  globalIgnores(['**/next.config.js', '**/*.test.js', '**/*.stories.js']),
+  {
+    extends: fixupConfigRules(
+      compat.extends(
+        'next/core-web-vitals',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/recommended',
+        'prettier',
+        'plugin:storybook/recommended'
+      )
+    ),
+    plugins: {
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
+      import: fixupPluginRules(_import),
+      'unused-imports': unusedImports
+    },
+    languageOptions: {
+      parser: tsParser
+    },
+    rules: {
+      '@next/next/no-img-element': 'off',
+      'unused-imports/no-unused-imports': 'error'
+    }
+  }
+]);
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 const withVanillaExtract = createVanillaExtractPlugin();
 
 const isProd = process.env.NODE_ENV === 'production';
@@ -20,4 +20,4 @@ const nextConfig = {
   }
 };
 
-module.exports = withVanillaExtract(nextConfig);
+export default withVanillaExtract(nextConfig);
