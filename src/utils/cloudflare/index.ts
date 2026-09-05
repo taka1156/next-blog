d38b549451d9542719204d62ceb87c4c98b56fd5
taka <tomo05061@gmail.com>
2026-09-06 01:51:47 +0900

    fix: 環境変数の見直し

--- src/utils/cloudflare/index.ts
+++ src/utils/cloudflare/index.ts
@@ -1,6 +1,9 @@
 export function getBaseUrl(): string {
-  if (process.env.CF_PAGES_BRANCH === 'main' && process.env.CUSTOM_DOMAIN) {
-    return `https://${process.env.CUSTOM_DOMAIN}`;
+  if (
+    process.env.CF_PAGES_BRANCH === 'main' &&
+    process.env.NEXT_PUBLIC_CUSTOM_DOMAIN
+  ) {
+    return `https://${process.env.NEXT_PUBLIC_CUSTOM_DOMAIN}`;
   }
   if (process.env.CF_PAGES_URL) {
     return process.env.CF_PAGES_URL;
