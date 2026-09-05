106e017e1aea8c67d8ddde8b5d14230262d0f266
taka <tomo05061@gmail.com>
2026-09-05 23:41:42 +0900

    fix: 本番OGPのベースURLの不具合修正

--- src/utils/cloudflare/index.ts
+++ src/utils/cloudflare/index.ts
@@ -1,8 +1,9 @@
 export function getBaseUrl(): string {
-  // 2. Cloudflare Pages環境なら、そのデプロイ固有のURLを使う
+  if (process.env.CF_PAGES_BRANCH === 'main' && process.env.CUSTOM_DOMAIN) {
+    return `https://${process.env.CUSTOM_DOMAIN}`;
+  }
   if (process.env.CF_PAGES_URL) {
     return process.env.CF_PAGES_URL;
   }
-
-  return process.env.BASE_URL;
+  return process.env.BASE_URL ?? 'http://localhost:3000';
 }
